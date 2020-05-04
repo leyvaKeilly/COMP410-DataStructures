953a95148ad602aa8806ce1008af04dfe2670efa
leyvaKeilly <lkeilly@yahoo.es>
2020-05-03 23:16:37 -0400

    no assignment 0

--- lkeilly_assignment0/lkeilly_assignment0/src/Assignment0/RunTests.java
+++
@@ -1,9 +0,0 @@
-package Assignment0;
-
-import gradingTools.comp410s19.assignment0.testcases.Assignment0Suite;
-
-public class RunTests {
-	public static void main(String[] args) {
-		Assignment0Suite.main(args);
-	}
-}
